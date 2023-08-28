9406fc1a00f3df96cd4d5b9fe0768d1635f4c8f8
Johannes Aamot-Skeidsvoll.com <82368148+Tmpecho@users.noreply.github.com>
2023-08-28 11:23:24 +0200

    Create GameOfLifeShort.py

---
+++ GameOfLifeShort.py
@@ -0,0 +1,13 @@
+import copy as C,random as R,time as T
+U,H,W,E,F=0.1,20,20,' ','#'
+def v(g,x,y,hx,hy): return 0<=x<H and 0<=y<W and (x,y)!=(hx,hy) and g[x][y]!=E
+def n(g,x,y): return sum(v(g,x+i,y+j,x,y) for i in [-1,0,1] for j in [-1,0,1])
+g=[[F if R.randint(1,5)==1 else E for _ in range(W)] for _ in range(H)]
+i=0
+while 1:
+    print('\n'.join([' '.join(r) for r in g]),f"\nGen:{i}\nPop:{sum(r.count(F) for r in g)}\n{'--'*W}")
+    T.sleep(0.1)
+    t=C.deepcopy(g)
+    for x in range(H):
+        for y in range(W): g[x][y]= E if t[x][y]==F and n(t,x,y) not in [2,3] else F if t[x][y]==E and n(t,x,y)==3 else g[x][y]
+    i+=1
